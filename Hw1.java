9a45a3443dce5b38f13d514a4db05fc69c7908e5
sjacinto11 <sjacinto@lsoc.org>
2025-09-04 19:41:05 +0000

    Hw 54b

---
+++ Hw1.java
@@ -0,0 +1,9 @@
+public class Hw1{
+  public static void main(String[] args){
+    double pi = 3.14;
+    double Radius = 3.712;
+    double area = pi * Radius;
+System.out.println(area);
+  }
+
+}
---
+++ Hw2.java
@@ -0,0 +1,12 @@
+public class Hw2 {
+  public static void main(String[] args){
+int x=475;
+int y=821;
+int z=369;
+int v=562;
+
+double average= (x+y+z+v) / 4;
+System.out.println(average);
+
+  }
+}
